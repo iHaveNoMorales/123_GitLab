a34ad869220f623fb2d9443f0a3300d1bb62cdda
iHaveNoMorales <cmorales2020@francisparker.org>
2023-10-19 20:24:15 -0700

    changed default in top N to 10

--- process_movie_data.py
+++ process_movie_data.py
@@ -3,7 +3,7 @@
 import csv
 
 
-def find_top_n(filename, n=5):
+def find_top_n(filename, n=10):
     """Finds the top n highest grossing movies in a CSV dataset.
        Input: filename, a string - points to filename of dataset
        Input: n, an int - the number of movies you want to print (defaults to 5)
